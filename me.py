631bf99a42c1580949eda8a410453791d61a9b6a
KZL <lz3450@outlook.com>
2023-09-13 12:25:18 -0500

    minor

--- me.py
+++ me.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     logger.info("VFG scale: (node: %d, edge: %d)", vfg.node_number, vfg.edge_number)
     # starting_nodes = [1482, 77486, 77491, 1546, 1551, 77533, 77528]
     # starting_nodes = [1482, 1546, 1551]
-    starting_nodes = [1482, 1546]
+    starting_nodes = [1479, 1543]
     logger.info("Starting nodes: %s", starting_nodes)
     model = extract_model(vfg, starting_nodes)
-    model.write("examples/tf2/model.dot")
+    model.write("examples/tf2/model.dot", 'Model')
