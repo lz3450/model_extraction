182cb24fa72948af5db3cc6d5d53ad2aedc77d0c
KZL <lz3450@outlook.com>
2023-09-20 18:53:42 -0500

    me.py

--- me.py
+++ me.py
@@ -5,11 +5,26 @@ logger = logging.getLogger(__name__)
 config_logger(logger)
 
 if __name__ == '__main__':
-    vfg = read_vfg('examples/tf2/vfg.dot')
+    # vfg = read_vfg('examples/tf2/vfg.dot')
+    # logger.info("VFG scale: (node: %d, edge: %d)", vfg.node_number, vfg.edge_number)
+    # starting_nodes = [1479, 1543]
+    # logger.info("Starting nodes: %s", starting_nodes)
+    # model = extract_model(vfg, starting_nodes)
+    # model.write("examples/tf2/model.dot", 'tf2')
+
+    vfg = read_vfg('examples/scale_publisher/vfg.dot')
     logger.info("VFG scale: (node: %d, edge: %d)", vfg.node_number, vfg.edge_number)
-    # starting_nodes = [1482, 77486, 77491, 1546, 1551, 77533, 77528]
-    # starting_nodes = [1482, 1546, 1551]
-    starting_nodes = [1479, 1543]
+    # starting_nodes = [795]
+    starting_nodes = [41733, 41727]
+    # starting_nodes = [1180]
+    # starting_nodes = [25185, 25174, 18543]
     logger.info("Starting nodes: %s", starting_nodes)
     model = extract_model(vfg, starting_nodes)
-    model.write("examples/tf2/model.dot", 'Model')
+    model.write("examples/scale_publisher/model.dot", 'scale_publisher')
+
+    # vfg = read_vfg('examples/turtle/vfg.dot')
+    # logger.info("VFG scale: (node: %d, edge: %d)", vfg.node_number, vfg.edge_number)
+    # starting_nodes = [180231]
+    # logger.info("Starting nodes: %s", starting_nodes)
+    # model = extract_model(vfg, starting_nodes)
+    # model.write("examples/turtle/model.dot", 'turtle')
