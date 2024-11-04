37e5cb25a999d8b685b992683c46faeae860eb67
KZL <lz3450@outlook.com>
2024-11-04 13:01:48 -0600

    json parser

--- model_builder/vfg.py
+++ model_builder/vfg.py
@@ -9,6 +9,7 @@ from __future__ import annotations
 from typing import Iterator
 import re
 import subprocess
+import json
 from tqdm import tqdm
 
 from .graph import Node, Edge, Graph
@@ -18,52 +19,57 @@ from .log import get_logger
 logger = get_logger(__name__)
 
 SHORT_TYPE: dict[str, str] = {
-    'Addr': 'a',
-    'Load': 'l',
-    'Store': 's',
-    'Gep': 'g',
-    'Copy': 'c',
-    'FormalParm': ')',
-    'ActualParm': '(',
-    'FormalRet': '<',
-    'ActualRet': '>',
-    'BinaryOP': 'b',
-    'UnaryOP': 'u',
-    'IntraPHI': 'p',
-    'Branch': '^',
-    'Cmp': '%',
-    'NullPtr': 'n',
-    'FormalINS': '+',
-    'FormalOUTS': '-',
-    'ActualINS': 'i',
-    'ActualOUTS': 'o',
-    'IntraMSSAPHIS': 'm'
+    "Addr": "a",
+    "Load": "l",
+    "Store": "s",
+    "Gep": "g",
+    "Copy": "c",
+    "FormalParm": ")",
+    "ActualParm": "(",
+    "FormalRet": "<",
+    "ActualRet": ">",
+    "BinaryOP": "b",
+    "UnaryOP": "u",
+    "IntraPHI": "p",
+    "Branch": "^",
+    "Cmp": "%",
+    "NullPtr": "n",
+    "FormalINS": "+",
+    "FormalOUTS": "-",
+    "ActualINS": "i",
+    "ActualOUTS": "o",
+    "IntraMSSAPHIS": "m",
 }
 
 PATTERNS: dict[str, re.Pattern] = {
-    'NodeLabelSeparation': re.compile(r',\\n\s*'),
-    'Function': re.compile(r'Function\[(\S+)\]'),
-    'BasicBlock': re.compile(r'BasicBlock\[(\S+)\]'),
-    'FunctionArgs': re.compile(r'.+ (\S+)'),
-    'Addr': re.compile(r'(\S+) = alloca (.+), align .+'),
-    'Load': re.compile(r'(%\S+) = load .+([%@]\S+),'),
-    'Store': re.compile(r'store \S+ (\S+), \S+ (%\S+),'),
-    'Gep': re.compile(r'(%\S+) = getelementptr inbounds (%\S+), (%\S+) (%\S+), (\S+) (\d+), (\S+) (\d+)'),
-    'Copy': re.compile(r'(%\S+) = (sitofp|fpext|bitcast|fptrunc) .+ (%\S+) to .+,'),
-    'FormalParm': re.compile(r'\S+ (%\S+)'),
-    'ActualParm': re.compile(r'(%\S+) = .+|\S+ (%\S+)'),
-    'ActualRet': re.compile(r'(%\S+) = (call|invoke) .+ @(\S+)\((.+)\)'),
-    'BinaryOP': re.compile(r'(%\S+) = (fmul|fadd) .+ (%\S+), (%\S+),'),
+    "NodeLabelSeparation": re.compile(r",\\n\s*"),
+    "Function": re.compile(r"Function\[(\S+)\]"),
+    "BasicBlock": re.compile(r"BasicBlock\[(\S+)\]"),
+    "FunctionArgs": re.compile(r".+ (\S+)"),
+    "Addr": re.compile(r"(\S+) = alloca (.+), align .+"),
+    "Load": re.compile(r"(%\S+) = load .+([%@]\S+),"),
+    "Store": re.compile(r"store \S+ (\S+), \S+ (%\S+),"),
+    "Gep": re.compile(r"(%\S+) = getelementptr inbounds (%\S+), (%\S+) (%\S+), (\S+) (\d+), (\S+) (\d+)"),
+    "Copy": re.compile(r"(%\S+) = (sitofp|fpext|bitcast|fptrunc) .+ (%\S+) to .+,"),
+    "FormalParm": re.compile(r"\S+ (%\S+)"),
+    "ActualParm": re.compile(r"(%\S+) = .+|\S+ (%\S+)"),
+    "ActualRet": re.compile(r"(%\S+) = (call|invoke) .+ @(\S+)\((.+)\)"),
+    "BinaryOP": re.compile(r"(%\S+) = (fmul|fadd) .+ (%\S+), (%\S+),"),
 }
 
 
 class VFGNode:
     def __init__(self, node: Node):
         self.name = node.name
-        self.info = PATTERNS['NodeLabelSeparation'].split(node.label.removeprefix('{').removesuffix('}'))
-        self.type, self.id = self._get_type_and_id()
+        info_json: dict[str, str] = json.loads(node.label.encode("utf-8").decode("unicode_escape").replace("\\<", "<"))
+        self.info = PATTERNS["NodeLabelSeparation"].split(node.label.removeprefix("{").removesuffix("}"))
+
+        self.type = info_json["type"].removesuffix("VFGNode")
+        self.id = int(info_json["id"])
         self.t = SHORT_TYPE[self.type]
-        self.ir, self.function, self.basic_block = self._get_ir()
+        self.ir = info_json.get("ir", "")
+        self.function = info_json.get("f", "")
+        self.basic_block = info_json.get("bb", "")
         # self.label = f'{self.type}({self.id})\\n{self._compile_info()}'
         self.compiled_info = self._compile_info()
         self.upper_nodes: set[VFGNode] = set()
@@ -71,169 +77,149 @@ class VFGNode:
 
     @staticmethod
     def demangle(mangled_name: str) -> str:
-        return subprocess.run(['c++filt', '-p', mangled_name], capture_output=True, text=True, check=True).stdout.strip().replace('<', '\\<').replace('>', '\\>')
-
-    def _get_type_and_id(self) -> tuple[str, int]:
-        type_and_id = self.info[0].split(" ID: ")
-        assert len(type_and_id) == 2, f'Invalid format for VFGNode "{self.name}" type and ID information'
-        return type_and_id[0].removesuffix('VFGNode'), int(type_and_id[1])
-
-    def _get_ir(self) -> tuple[str, str, str]:
-        ir = self.info[2]
-        f = ''
-        bb = ''
-
-        if not ir.startswith('(none)'):
-            if match := PATTERNS['Function'].search(ir):
-                f = VFGNode.demangle(match.group(1))
-            if match := PATTERNS['BasicBlock'].search(ir):
-                bb = match.group(1)
-        elif self.type == 'FormalRet':
-            if match := PATTERNS['Function'].search(self.info[1]):
-                f = VFGNode.demangle(match.group(1))
-        else:
-            ir = ''
-            if self.type != 'NullPtr':
-                logger.warning('%s (%d) does not contains IR code', self.type, self.id)
-
-        return ir, f, bb
+        return (
+            subprocess.run(["c++filt", "-p", mangled_name], capture_output=True, text=True, check=True)
+            .stdout.strip()
+            .replace("<", "\\<")
+            .replace(">", "\\>")
+        )
 
     def _compile_info(self) -> str:
         if not self.ir:
-            return ''
+            return ""
         match self.type:
-            case 'Addr':
+            case "Addr":
                 if match := PATTERNS[self.type].match(self.ir):
                     self._var_type = match.group(2)
                     self._ptr = match.group(1)
                     return f"{self._var_type} {self._ptr}"
-            case 'Load':
+            case "Load":
                 if match := PATTERNS[self.type].match(self.ir):
                     self._ptr = match.group(2)
                     self._var = match.group(1)
-                    return f'{self._ptr} → {self._var}'
-            case 'Store':
+                    return f"{self._ptr} → {self._var}"
+            case "Store":
                 if match := PATTERNS[self.type].match(self.ir):
                     self._var = match[1]
                     self._ptr = match[2]
-                    return f'{self._var} → {self._ptr}'
-            case 'Gep':
+                    return f"{self._var} → {self._ptr}"
+            case "Gep":
                 if match := PATTERNS[self.type].match(self.ir):
                     self._elementptr = match.group(1)
                     self._ptr = match.group(4)
-                    return f'{self._ptr}.{self._elementptr}'
-            case 'Copy':
+                    return f"{self._ptr}.{self._elementptr}"
+            case "Copy":
                 if match := PATTERNS[self.type].match(self.ir):
                     self._from_var = match[3]
                     self._to_var = match[1]
-                    return f'{self._from_var} → {self._to_var}'
-            case 'FormalParm':
+                    return f"{self._from_var} → {self._to_var}"
+            case "FormalParm":
                 if match := PATTERNS[self.type].match(self.ir):
                     self._parm = match.group(1)
                     return self._parm
-            case 'ActualParm':
+            case "ActualParm":
                 if match := PATTERNS[self.type].match(self.ir):
                     group1, group2 = match.groups()
                     self._parm = group1 if group1 else group2
                     return self._parm
-            case 'FormalRet':
+            case "FormalRet":
                 return self.function
-            case 'ActualRet':
+            case "ActualRet":
                 self._args: list[str] = []
                 if match := PATTERNS[self.type].match(self.ir):
                     self._retval = match[1]
                     self._fnptrval = match[3]
                     args = match[4]
-                    for arg in args.split(', '):
-                        if arg_match := PATTERNS['FunctionArgs'].match(arg):
+                    for arg in args.split(", "):
+                        if arg_match := PATTERNS["FunctionArgs"].match(arg):
                             self._args.append(arg_match[1])
                     return f"{self._retval} = {VFGNode.demangle(self._fnptrval)}({', '.join(self._args)})"
-            case 'BinaryOP':
+            case "BinaryOP":
                 if match := PATTERNS[self.type].match(self.ir):
                     self._binary_op_parms = (match[3], match[4])
                     return f'{match[2]}({", ".join(self._binary_op_parms)}) → {match[1]}'
-            case 'UnaryOP':
+            case "UnaryOP":
                 pass
-            case 'IntraPHI':
+            case "IntraPHI":
                 return self.function
-            case 'Branch':
+            case "Branch":
                 pass
-            case 'Cmp':
+            case "Cmp":
                 pass
-            case 'FormalINS':
+            case "FormalINS":
                 pass
-            case 'FormalOUTS':
+            case "FormalOUTS":
                 pass
-            case 'ActualINS':
+            case "ActualINS":
                 pass
-            case 'ActualOUTS':
+            case "ActualOUTS":
                 pass
-            case 'IntraMSSAPHIS':
+            case "IntraMSSAPHIS":
                 pass
             case _:
-                raise ValueError(f'Unknown VFG node type {self.type}')
-        return r'\n'.join(self.info)
+                raise ValueError(f"Unknown VFG node type {self.type}")
+        return r"\n".join(self.info)
 
     def __repr__(self) -> str:
-        return f'{self.type}({self.id}, {self.name})'
+        return f"{self.type}({self.id}, {self.name})"
 
     @property
     def var_type(self) -> str:
-        if self.t != 'a':
+        if self.t != "a":
             raise AttributeError(f'"{self.type}" does not have `var_type` attribute.')
         return self._var_type
 
     @property
     def ptr(self) -> str:
-        if self.t not in 'alsg':
+        if self.t not in "alsg":
             raise AttributeError(f'"{self.type}" does not have `ptr` attribute.')
         return self._ptr
 
     @property
     def var(self) -> str:
-        if self.t not in 'ls':
+        if self.t not in "ls":
             raise AttributeError(f'"{self.type}" does not have `var` attribute.')
         return self._var
 
     @property
     def elementptr(self) -> str:
-        if self.t != 'g':
+        if self.t != "g":
             raise AttributeError(f'"{self.type}" does not have `elementptr` attribute.')
         return self._elementptr
 
     @property
     def from_var(self) -> str:
-        if self.t != 'c':
+        if self.t != "c":
             raise AttributeError(f'"{self.type}" does not have `from_var` attribute.')
         return self._from_var
 
     @property
     def to_var(self) -> str:
-        if self.t != 'c':
+        if self.t != "c":
             raise AttributeError(f'"{self.type}" does not have `to_var` attribute.')
         return self._to_var
 
     @property
     def parm(self) -> str:
-        if self.t not in '()':
+        if self.t not in "()":
             raise AttributeError(f'"{self.type}" does not have `parm` attribute.')
         return self._parm
 
     @property
     def parms(self) -> tuple[str, str]:
-        if self.t != 'b':
+        if self.t != "b":
             raise AttributeError(f'"{self.type}" does not have `parms` attribute.')
         return self._binary_op_parms
 
     @property
     def retval(self) -> str:
-        if self.t != '>':
+        if self.t != ">":
             raise AttributeError(f'"{self.type}" does not have `retval` attribute.')
         return self._retval
 
     @property
     def args(self) -> list[str]:
-        if self.t != '>':
+        if self.t != ">":
             raise AttributeError(f'"{self.type}" does not have `args` attribute.')
         return self._args
 
@@ -256,8 +242,10 @@ class VFGNode:
 
 class VFG:
     def __init__(self, graph: Graph) -> None:
-        self.nodes: dict[str, VFGNode] = {node.name: VFGNode(node) for node in tqdm(graph.nodes, bar_format='{l_bar}{bar:50}{r_bar}', unit=' nodes')}
-        for edge in tqdm(graph.edges, bar_format='{l_bar}{bar:50}{r_bar}', unit=' edges'):
+        self.nodes: dict[str, VFGNode] = {
+            node.name: VFGNode(node) for node in tqdm(graph.nodes, bar_format="{l_bar}{bar:50}{r_bar}", unit=" nodes")
+        }
+        for edge in tqdm(graph.edges, bar_format="{l_bar}{bar:50}{r_bar}", unit=" edges"):
             self.nodes[edge.source].add_lower_nodes(self.nodes[edge.target])
             self.nodes[edge.target].add_upper_nodes(self.nodes[edge.source])
 
@@ -268,7 +256,7 @@ class VFG:
         yield from sorted(self.nodes.values(), key=lambda node: node.id)
 
     def __repr__(self) -> str:
-        return f'VFG({self.node_number},{self.edge_number})'
+        return f"VFG({self.node_number},{self.edge_number})"
 
     @property
     def node_number(self) -> int:
@@ -294,11 +282,7 @@ class VFG:
     def search_nodes(self, type: str, function: str, basic_block: str) -> set[VFGNode]:
         matched_nodes: set[VFGNode] = set()
         for node in self:
-            if (
-                node.type == type and
-                node.function == function and
-                node.basic_block == basic_block
-            ):
+            if node.type == type and node.function == function and node.basic_block == basic_block:
                 matched_nodes.add(node)
         return matched_nodes
 
