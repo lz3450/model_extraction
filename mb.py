e523db349e671a3d3c8aeeccb95771596abdfff3
KZL <lz3450@outlook.com>
2024-11-04 11:00:11 -0600

    mb and me

--- mb.py
+++ mb.py
@@ -1,16 +1,18 @@
-from model_builder import get_logger, Graph, VFG, Model
+from model_builder import get_logger, VFG, Model
 
 logger = get_logger(__name__)
 
 
 def scale_publisher():
-    # vfg = VFG.from_file('examples/scale_publisher/full_svfg.dot')
+    starting_node_ids = {1158, 709, 658, 662, 18249, 39268, 41755}
+    logger.info("Starting nodes: %s", starting_node_ids)
+
     vfg = VFG.from_file('examples/scale_publisher/vfg.dot')
     logger.info("VFG scale: %s", vfg.size)
-    starting_node_ids = {664, 668, 41733, 41727}
-    logger.info("Starting nodes: %s", starting_node_ids)
-    model = Model(vfg, starting_node_ids)
-    model.write('examples/scale_publisher/model.dot', 'scale_publisher model', 'scale_publisher model')
+    logger.info("Building model from VFG...")
+
+    vfg_model = Model(vfg, starting_node_ids)
+    vfg_model.write('examples/scale_publisher/vfg_model.dot', 'scale_publisher model', 'scale_publisher model')
 
 
 def tf2():
@@ -22,8 +24,12 @@ def tf2():
     vfg = VFG.from_file('examples/tf2/vfg.dot')
     logger.info("VFG scale: %s", vfg.size)
     logger.info("Building model from VFG...")
-    vfg_model = Model(vfg, starting_node_ids)
-    vfg_model.write('examples/tf2/vfg_model.dot', 'tf2 model', 'tf2 model')
+
+    original_model = Model(vfg, starting_node_ids, False)
+    original_model.write('examples/tf2/model_original.dot', 'tf2 model', 'tf2 model')
+
+    vfg_model = Model(vfg, starting_node_ids, True)
+    vfg_model.write('examples/tf2/model_vfg.dot', 'tf2 model', 'tf2 model')
 
     # svfg = VFG.from_file('examples/tf2/full_svfg.dot')
     # logger.info("SVFG scale: %s", svfg.size)
@@ -33,16 +39,21 @@ def tf2():
 
 
 def turtle():
-    starting_node_ids = {180231}
+    starting_node_ids = {45757, 45758, 45759}
     logger.info("Starting nodes: %s", starting_node_ids)
 
     vfg = VFG.from_file('examples/turtle/vfg.dot')
     logger.info("VFG scale: %s", vfg.size)
-    model = Model(vfg, starting_node_ids)
-    model.write('examples/turtle/model.dot', 'turtle model', 'turtle model')
+    logger.info("Building model from VFG...")
+
+    original_model = Model(vfg, starting_node_ids, False)
+    original_model.write('examples/turtle/model_original.dot', 'turtle model', 'turtle model')
+
+    vfg_model = Model(vfg, starting_node_ids)
+    vfg_model.write('examples/turtle/vfg_model.dot', 'turtle model', 'turtle model')
 
 
 if __name__ == '__main__':
     # scale_publisher()
-    tf2()
-    # turtle()
+    # tf2()
+    turtle()
--- me.py
+++ me.py
@@ -5,22 +5,22 @@ logger = logging.getLogger(__name__)
 config_logger(logger)
 
 if __name__ == '__main__':
-    # vfg = read_vfg('examples/tf2/vfg.dot')
-    # logger.info("VFG scale: (node: %d, edge: %d)", vfg.node_number, vfg.edge_number)
-    # starting_nodes = [1479, 1543]
-    # logger.info("Starting nodes: %s", starting_nodes)
-    # model = extract_model(vfg, starting_nodes)
-    # model.write("examples/tf2/model.dot", 'tf2')
-
-    vfg = read_vfg('examples/scale_publisher/vfg.dot')
+    vfg = read_vfg('examples/tf2/vfg.dot')
     logger.info("VFG scale: (node: %d, edge: %d)", vfg.node_number, vfg.edge_number)
-    # starting_nodes = [795]
-    starting_nodes = [41733, 41727]
-    # starting_nodes = [1180]
-    # starting_nodes = [25185, 25174, 18543]
+    starting_nodes = [1479, 1543]
     logger.info("Starting nodes: %s", starting_nodes)
     model = extract_model(vfg, starting_nodes)
-    model.write("examples/scale_publisher/model.dot", 'scale_publisher')
+    model.write("examples/tf2/model.dot", 'tf2')
+
+    # vfg = read_vfg('examples/scale_publisher/vfg.dot')
+    # logger.info("VFG scale: (node: %d, edge: %d)", vfg.node_number, vfg.edge_number)
+    # # starting_nodes = [795]
+    # starting_nodes = [41733, 41727]
+    # # starting_nodes = [1180]
+    # # starting_nodes = [25185, 25174, 18543]
+    # logger.info("Starting nodes: %s", starting_nodes)
+    # model = extract_model(vfg, starting_nodes)
+    # model.write("examples/scale_publisher/model.dot", 'scale_publisher')
 
     # vfg = read_vfg('examples/turtle/vfg.dot')
     # logger.info("VFG scale: (node: %d, edge: %d)", vfg.node_number, vfg.edge_number)
